214de4d30e3eb40b56ae1baa1248a36e58f196e6
ewalsh <ewalsh@brandeis.edu>
2020-10-24 14:40:33 +0100

    added default db empty string

--- py_code/cassconn.py
+++ py_code/cassconn.py
@@ -3,7 +3,7 @@ from cassandra.cluster import Cluster
 #if __name__ == "__main__":
 
 # define cassandra connection function
-def create_session(host, port, db):
+def create_session(host, port, db = ''):
     cluster = Cluster([host],port=port)
     session = cluster.connect(db,wait_for_all_pools=True)
     return(session)
