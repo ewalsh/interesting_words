a894a61f4e844a0dec55e82f59b26bf2b96b1983
ewalsh <edmund.walsh.200@gmail.com>
2020-10-24 23:33:24 +0100

    testing within zeppelin

--- py_code/cassconn.py
+++ py_code/cassconn.py
@@ -7,3 +7,6 @@ def create_session(host, port, db = ''):
     cluster = Cluster([host],port=port)
     session = cluster.connect(db,wait_for_all_pools=True)
     return(session)
+
+
+session = create_session('172.29.0.2', 9042, 'iwords')
--- py_code/speech_parse.py
+++ py_code/speech_parse.py
@@ -1,6 +1,6 @@
 import numpy
 import pendulum
-from cassconn import create_session
+from cassconn import session
 
 def date_check(dt_txt):
     if dt_txt == '':
@@ -11,12 +11,22 @@ def date_check(dt_txt):
 
 # define text parser for presidential speeches
 def parse_ps(filename, name, doc_num):
-    with open(filename, 'r', encoding='mbcs') as fobj:
+    with open(filename, 'r', encoding='ISO-8859-1') as fobj:
         raw_title = fobj.readline()
         parsed_title = raw_title.strip().replace('<title=','').replace('>','').replace('"','')
         raw_date = fobj.readline()
         parsed_date_txt = raw_date.strip().replace('<date=','').replace('>','').replace('"','')
         parsed_date = date_check(parsed_date_txt)
-        print(filename)
-        print(parsed_title)
-        print(parsed_date)
+        counter = 0
+        line1 = fobj.readline()
+        while line1:
+            session.execute(
+                """
+                INSERT INTO iwords.raw (filename, doc_num, line_num, pres, text)
+                VALUES ('{}', {}, {}, '{}', '{}')
+                """.format(filename, doc_num, counter, d, line1)
+            )
+            counter = counter + 1
+            print(filename)
+            print(parsed_title)
+            print(parsed_date)
