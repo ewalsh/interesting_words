fe39d3a0a1f2dfdb5d47741e3bdcfc568b86af9f
ewalsh <edmund.walsh.200@gmail.com>
2020-10-25 09:40:47 +0000

    back to slimmed size, with changes

--- py_code/speech_parse.py
+++ py_code/speech_parse.py
@@ -2,6 +2,7 @@ import numpy
 import pendulum
 from cassconn import session
 
+
 def date_check(dt_txt):
     if dt_txt == '':
         return(None)
@@ -13,20 +14,22 @@ def date_check(dt_txt):
 def parse_ps(filename, name, doc_num):
     with open(filename, 'r', encoding='ISO-8859-1') as fobj:
         raw_title = fobj.readline()
-        parsed_title = raw_title.strip().replace('<title=','').replace('>','').replace('"','')
+        parsed_title = raw_title.strip().replace('<title=','').replace('>','').replace('"','').replace("'","''")
         raw_date = fobj.readline()
         parsed_date_txt = raw_date.strip().replace('<date=','').replace('>','').replace('"','')
         parsed_date = date_check(parsed_date_txt)
         counter = 0
-        line1 = fobj.readline()
-        while line1:
-            session.execute(
-                """
-                INSERT INTO iwords.raw (filename, doc_num, line_num, pres, text)
-                VALUES ('{}', {}, {}, '{}', '{}')
-                """.format(filename, doc_num, counter, d, line1)
-            )
+        line0 = fobj.readline()
+        while line0:
+            line0 = fobj.readline()
+            line1 = line0.replace("'","''")
+            stmt = ''
+            if (line1 != '') & (line1.strip() != ''):
+                if parsed_date == None:
+                    stmt = """INSERT INTO iwords.raw (filename, doc_num, line_num, pres, text, speech_title) VALUES ('{}', {}, {}, '{}', '{}', '{}');""".format(filename, doc_num, counter, name, line1, parsed_title)
+                else:
+                    stmt = """INSERT INTO iwords.raw (filename, doc_num, line_num, pres, text, speech_title, speech_dt) VALUES ('{}', {}, {}, '{}', '{}', '{}', '{}');""".format(filename, doc_num, counter, name, line1, parsed_title, parsed_date)
+                print(stmt)
+                session.execute(stmt)
             counter = counter + 1
-            print(filename)
-            print(parsed_title)
-            print(parsed_date)
+            line1 = fobj.readline()
